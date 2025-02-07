7429dc035fe4237d889621702c205fe040de6b02
Luoxia-zhang <luoxia2675128493@gmail.com>
2025-02-07 21:30:13 +0800

    NJU-ProjectN/nemu ics2024 initialized
    NJU-ProjectN/nemu e69f07f1a5bb4e754fd932eb5c668857543ca2eb x86,inst: fix dnpc

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "../obj_dir/Vtop.h"
+#include <assert.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vtop* top;
+
+void step_and_dump_wave(){
+	top->eval();
+	contextp->timeInc(1);
+	tfp->dump(contextp->time());
+}
+
+int main(){
+	contextp = new VerilatedContext;
+	tfp = new VerilatedVcdC;
+	top = new Vtop;
+	contextp->traceEverOn(true);
+	top->trace(tfp,0);
+	tfp->open("dump.vcd");
+	
+  top->y=0b00;  top->x0=0b11; top->x1=0b01; top->x2=0b00;  top->x3=0b10; step_and_dump_wave();
+                top->x0=0b10; top->x1=0b01; top->x2=0b01;  top->x3=0b00; step_and_dump_wave();
+  top->y=0b01;  top->x0=0b11; top->x1=0b01; top->x2=0b00;  top->x3=0b10; step_and_dump_wave();
+                top->x0=0b10; top->x1=0b01; top->x2=0b01;  top->x3=0b00; step_and_dump_wave();
+  top->y=0b10;  top->x0=0b11; top->x1=0b01; top->x2=0b00;  top->x3=0b10; step_and_dump_wave();
+                top->x0=0b10; top->x1=0b01; top->x2=0b01;  top->x3=0b00; step_and_dump_wave();
+  top->y=0b11;  top->x0=0b11; top->x1=0b01; top->x2=0b00;  top->x3=0b10; step_and_dump_wave();
+                top->x0=0b10; top->x1=0b01; top->x2=0b01;  top->x3=0b00; step_and_dump_wave();
+	
+	step_and_dump_wave();
+	tfp->close();
 }
